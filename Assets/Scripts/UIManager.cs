80cb1ef862285cb5aea0e769647e4987de9810fc
Red Williams <redpwilliams@gmail.com>
2023-02-22 02:05:17 -0500

    start screen canvas w variable props

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,23 +5,31 @@ using System;
 public class UIManager : MonoBehaviour
 {
   private float mileage;
+
+  [Header("Mileage Props")]
   [SerializeField] private Text mileageText;
-  [SerializeField] private float padding;
+  [SerializeField] private float stepsPadding;
   [SerializeField] private float stepsMultiplier;
 
+  [Header("Start Screen Props")]
+  [SerializeField] private GameObject startScreen;
+  [SerializeField] private Vector2 origin;
+
+
   // Used for scroll velocity
+  [Header("BGM Instance")]
   [SerializeField] private BackgroundManager bgm;
+
+  // UIManager RectTransform componenet
+  private RectTransform rt;
 
   void Start()
   {
-    padding = 50f;
-    stepsMultiplier = 3.0f;
-    mileage = 0f;
-    mileageText = mileageText.GetComponent<Text>();
-    RectTransform mrt = mileageText.GetComponent<RectTransform>();
-    mrt.anchoredPosition = new Vector2(-padding, mrt.anchoredPosition.y - mrt.rect.height);
-
     bgm = bgm.GetComponent<BackgroundManager>();
+    rt = GetComponent<RectTransform>();
+
+    InitMileage();
+    InitStartScreen();
   }
 
   void Update()
@@ -34,5 +42,27 @@ public class UIManager : MonoBehaviour
   {
     // Update mileage
     mileageText.text = String.Format("{0:#} steps", mileage);
+  }
+
+  void InitMileage()
+  {
+    stepsPadding = 50f;
+    stepsMultiplier = 3.0f;
+    mileage = 0f;
+    mileageText = mileageText.GetComponent<Text>();
+    RectTransform mileageRT = mileageText.GetComponent<RectTransform>();
+    mileageRT.anchoredPosition = new Vector2(-stepsPadding, mileageRT.anchoredPosition.y - mileageRT.rect.height);
+  }
+
+  void InitStartScreen()
+  {
+    RectTransform startScreenRT = startScreen.GetComponent<RectTransform>();
+
+    // Width and Height
+    float width = rt.rect.width * 0.8f; // 80% of parent
+    float height = rt.rect.height * 0.8f; // 80% of parent
+
+    // Set
+    startScreenRT.rect.Set(origin.x, origin.y, width, height);
   }
 }
