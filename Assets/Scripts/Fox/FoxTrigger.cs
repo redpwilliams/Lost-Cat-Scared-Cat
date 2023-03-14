12cb2a7f37b71fa9642ad8606d18c13a7c75a426
Red Williams <redpwilliams@gmail.com>
2023-03-13 22:44:29 -0400

    set trigger bc size to parent size

--- Assets/Scripts/Fox/FoxTrigger.cs
+++ Assets/Scripts/Fox/FoxTrigger.cs
@@ -2,12 +2,9 @@ using UnityEngine;
 
 public class FoxTrigger : MonoBehaviour
 {
-    void Start()
-    {
-        Debug.Log("Trigger initialized");
-    }
+    
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("Fox trigger activated");
+        Debug.Log("Fox trigger activated: " + col.gameObject.name);
     }
 }
