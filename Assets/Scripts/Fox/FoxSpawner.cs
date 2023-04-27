e72b60c61059cfda5fa9b484af53c1c21d5f8ae3
Red Williams <redpwilliams@gmail.com>
2023-04-27 16:54:00 -0400

    successfully integrated white fox!

--- Assets/Scripts/Fox/FoxSpawner.cs
+++ Assets/Scripts/Fox/FoxSpawner.cs
@@ -7,15 +7,15 @@ public class FoxSpawner : MonoBehaviour
   private float _startX = 4.5f;
   private float _startY = -0.875f;
   private Transform _transform;
-
   
-  [SerializeField] private GameObject rf;
-  [SerializeField] private GameObject sf;
+  [SerializeField] private GameObject redFox;
+  [SerializeField] private GameObject whiteFox;
 
   void Start()
   {
     _transform = GetComponent<Transform>();
     _transform.localPosition = new Vector3(_startX, _startY, _transform.position.z);
+
     StartCoroutine(SpawnFoxes());
   }
 
@@ -32,13 +32,22 @@ public class FoxSpawner : MonoBehaviour
 
   void InstantiateRandomFox()
   {
+    GameObject foxClone;
+    string s = "I am a ";
+
     if (Random.value < 0.5f)
     {
       // Create RunningFox
-      Instantiate<GameObject>(rf, _transform.position, _transform.rotation);
-    return;
-    } 
-      Instantiate<GameObject>(sf, _transform.position, _transform.rotation);
+      foxClone = Instantiate<GameObject>(redFox, _transform.position, _transform.rotation);
+      foxClone.name = "Red Fox (Clone)";
+      s += "Red Fox";
+    }
+    else  
+    {
+      foxClone = Instantiate<GameObject>(whiteFox, _transform.position, _transform.rotation);
+      foxClone.name = "White Fox (Clone)";
+      s += "White Fox";
+    }
+    Debug.Log(s);
   }
-
 }
