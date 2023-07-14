add858ab1f19ae64f03e8c0de7b70b32e011975f
Red Williams <redpwilliams@gmail.com>
2023-07-14 02:25:42 -0400

    bring Character up to code standard

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -30,67 +30,50 @@ public abstract class Character : MonoBehaviour
     this.HasInputJump = false;
     this.IsVisiblyJumping = false;
   }
-  
-  // protected virtual void Start()
-  // {
-  // }
-
-  /// <summary>
   /// Returns whether or not the character is running
-  /// </summary>
-  /// <returns>isGrounded, bool value of character grounded status</returns>
   protected virtual bool IsRunning()
   {
     return this.IsGrounded && Mathf.Abs(this.rb.velocity.x) > 0.1f;
   }
 
-  /// <summary>
   /// Returns the bool of the Character jumping 
   /// This is independent from the animation (animation relies on this/math)
-  /// </summary>
-  /// <returns>Status of Character jumping</returns>
   protected virtual bool IsJumping()
   {
     return rb.velocity.y > 0.01f;
   }
 
-  /// <summary>
   ///  If GameObject is falling. Is independent from animation
-  /// </summary>
-  /// <returns>
-  ///  rb.velocity.y, bool state if the Character is falling
-  /// </returns>
   protected bool IsFalling()
   {
     return rb.velocity.y < -0.1f;
   }
 
+  /// Sets the passed `IsRunning()` parameter in the Animator tab
   protected void SetRunAnimationParam(bool value)
   {
     this.anim.SetBool(Running, value);
   }
 
+  /// Sets the passed `IsJumping()` parameter in the Animator tab
   protected void SetJumpAnimationParam(bool value)
   {
     this.anim.SetBool(Jumping, value);
   }
 
+  /// Sets the passed `IsFalling()` parameter in the Animator tab
   protected void SetFallAnimationParam(bool value)
   {
     this.anim.SetBool(Falling, value);
   }
   
-  /// <summary>
   /// Called when this object collides with something
-  /// </summary>
   private void OnCollisionEnter2D()
   {
     this.IsGrounded = true;
   }
 
-  /// <summary>
   /// Called when this object leaves a collider
-  /// </summary>
   private void OnCollisionExit2D()
   {
     this.IsGrounded = false;
