23cf15b30d8f6adf1accde50fc1d172f8e22f4f6
Red Williams <redpwilliams@gmail.com>
2023-07-16 14:14:00 -0400

    brown fox stays idle and moving during jump prep

--- Assets/Scripts/Fox/BrownFox.cs
+++ Assets/Scripts/Fox/BrownFox.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class BrownFox : Fox
 {
-
     private new Transform transform;
 
     protected override void Awake()
@@ -13,14 +12,14 @@ public class BrownFox : Fox
     
     protected override void Update()
     {
-        
         base.Update();
         
-        // Carry on with normal Run if has finished Attack
-        if (HasAttacked) return;
+        // Carry on with normal Run if has started Attack
+        if (IsVisiblyJumping) return;
 
         // Else, move when idle   
-        float idleSpeed = BackgroundManager.bgm.GetScrollVelocity() * 1.1f;
+        float idleSpeed = BackgroundManager.bgm.GetScrollVelocity() * 1.1f; // FIXME - Get 1.1f dynamically
+        // ^ Singleton?
         Vector3 currentPosition = transform.position;
         this.transform.position = new Vector3(currentPosition.x - idleSpeed * Time.deltaTime,
                 currentPosition.y, currentPosition.z);
@@ -28,7 +27,8 @@ public class BrownFox : Fox
     
     protected override void HandleMovement()
     {
-        if (!this.HasAttacked) return;
+        // Stay crouching if the following is true
+        if (!this.IsVisiblyJumping) return;
         
         // Keep velocity
         this.rb.velocity = new Vector2(this.RunSpeed, this.rb.velocity.y);
