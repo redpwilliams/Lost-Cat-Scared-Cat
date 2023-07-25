6b587755a472bb43ca37e7fd755669cdeb4bce35
Red Williams <redpwilliams@gmail.com>
2023-07-25 00:25:00 -0400

    working menu switcher

---
+++ Assets/Scripts/Game Managers/ButtonManager.cs
@@ -0,0 +1,28 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public sealed class ButtonManager : MonoBehaviour
+{
+
+    [UsedImplicitly]
+    private void HandlePlay()
+    {
+        
+    }
+
+    [UsedImplicitly]
+    private void HandleOptions()
+    {
+    }
+
+    [UsedImplicitly]
+    private void HandleQuit()
+    {
+    }
+
+    [UsedImplicitly]
+    private void HandleReturn()
+    {
+    }
+}
