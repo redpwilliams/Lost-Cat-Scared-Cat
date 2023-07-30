3720cdf3480b9e9b9e39c798e2253364d06d0ab2
Red Williams <redpwilliams@gmail.com>
2023-07-30 12:59:28 -0500

    add customizable line length

--- Assets/Scripts/UI/LineController.cs
+++ Assets/Scripts/UI/LineController.cs
@@ -7,7 +7,13 @@ public class LineController : MonoBehaviour
     private GameObject _player;
 
     /// How thin the line should be
-    [SerializeField] private float lineWidth = 0.02f;
+    [SerializeField] private float lineWidth = 0.2f;
+
+    // How long the line is
+    [SerializeField] private float lineLength = 0.3f;
+    
+    // Gap between Player sprite and speech line start
+    [SerializeField] private float playerLineOffset = 2f;
 
     /// Minimum angle the line should make with the horizontal
     private const float MinAngle = Mathf.PI / 6; // 30 deg
@@ -21,22 +27,31 @@ public class LineController : MonoBehaviour
     /// Right-side divisor, marking where the line should switch directions
     private const float PositiveWedge = 1f;
 
+    /// Makes using/visualizing numbers in the Editor a little easier
+    private const float ParameterScaleFactor = 0.1f;
+
     private void OnEnable()
     {
         EventManager.Events.OnPauseKeyDown += (b =>
         {
             gameObject.SetActive(b);
+            
+            // Continue with vector calculations when game is paused
             if (!b) return;
-            float lineLength = 0.3f;
+            
             Vector3 playerPosition = _player.transform.position;
+            Vector3 direction = ChooseDirection(playerPosition.x);
 
-            Vector3 direction = ChooseEndpoint(playerPosition.x);
+            // Calculate start position with an gap/offset from the player
+            Vector3 startPosition = playerPosition + direction *
+                (playerLineOffset * ParameterScaleFactor);
 
             // Calculate the end position based on the line length and player's position
-            Vector3 endPosition = playerPosition + direction * lineLength;
+            Vector3 endPosition = startPosition + direction * (lineLength * 
+                ParameterScaleFactor);
 
             // Set the positions of the line renderer
-            _lr.SetPosition(0, playerPosition);
+            _lr.SetPosition(0, startPosition);
             _lr.SetPosition(1, endPosition);
         });
     }
@@ -44,7 +59,7 @@ public class LineController : MonoBehaviour
     private void Awake()
     {
         _lr = GetComponent<LineRenderer>();
-        SetWidth(lineWidth);
+        SetWidth(lineWidth * ParameterScaleFactor);
         _player = GameObject.FindWithTag("Player");
     }
 
@@ -59,7 +74,7 @@ public class LineController : MonoBehaviour
         _lr.endWidth = width;
     }
 
-    private static Vector3 ChooseEndpoint(float playerXPos)
+    private static Vector3 ChooseDirection(float playerXPos)
     {
         float theta = Random.Range(MinAngle, MaxAngle);
         float dir = playerXPos is < NegativeWedge or < PositiveWedge and >= 0 ? 1f : -1f;
