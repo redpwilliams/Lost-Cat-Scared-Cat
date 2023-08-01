3ef998fb701257670be81afceb657190007d1724
Red Williams <redpwilliams@gmail.com>
2023-08-01 01:26:26 -0500

    end refactor

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,21 +12,21 @@ public class Player : Character, IFlashable
     internal const int NumLives = 9;
 
     [Header("Movement Parameters")] 
-    [SerializeField] private float jumpForce;
+    [SerializeField] private float _jumpForce;
 
-    [SerializeField] private float topSpeed;
-    [SerializeField] private float acceleration = 4f;
+    [SerializeField] private float _topSpeed;
+    [SerializeField] private float _acceleration = 4f;
 
-    [SerializeField] private float deceleration = 10f; // Bigger value = harder stop
+    [SerializeField] private float _deceleration = 10f; // Bigger value = harder stop
 
     /// How much to change the gravity when Player is falling
     [Range(1, 5)]
-    [SerializeField] private float fallGravityMultiplier;
+    [SerializeField] private float _fallGravityMultiplier;
 
     [Header("Flash Animation Parameters")]
-    [SerializeField] private int flashCount = 5;
-    [SerializeField] private float flickDuration = 0.1f;
-    [SerializeField] private Color flashColor;
+    [SerializeField] private int _flashCount = 5;
+    [SerializeField] private float _flickDuration = 0.1f;
+    [SerializeField] private Color _flashColor;
     private bool _isInvincible;
 
     /// "Normal" gravity when not jumping
@@ -37,14 +37,14 @@ public class Player : Character, IFlashable
 
     /// Cat skins
     [SerializeField] 
-    private SpriteLibraryAsset[] sprites = new SpriteLibraryAsset[5];
+    private SpriteLibraryAsset[] _sprites = new SpriteLibraryAsset[5];
 
     private void OnValidate()
     {
-        if (sprites.Length == 5) return;
+        if (_sprites.Length == 5) return;
         
         Debug.LogWarning("\"Sprites\" array must be of length 5");
-        Array.Resize(ref this.sprites, 5);
+        Array.Resize(ref this._sprites, 5);
 
     }
     
@@ -65,7 +65,7 @@ public class Player : Character, IFlashable
         _legacyGravityScale = rb.gravityScale;
 
         SpriteLibrary sl = GetComponent<SpriteLibrary>();
-        sl.spriteLibraryAsset = sprites[SaveSystem.LoadPreferences().CatID - 1];
+        sl.spriteLibraryAsset = _sprites[SaveSystem.LoadPreferences().CatID - 1];
     }
 
     protected void Start()
@@ -117,11 +117,11 @@ public class Player : Character, IFlashable
     private void HandleRunInput(int inputDirection)
     {
         // Force-based movement
-        float targetVelocity = inputDirection * topSpeed;
+        float targetVelocity = inputDirection * _topSpeed;
         float speedDiff = targetVelocity - rb.velocity.x;
         float accelerationRate = (Mathf.Abs(targetVelocity) > 0.01f)
-            ? acceleration
-            : deceleration;
+            ? _acceleration
+            : _deceleration;
         float movement = Mathf.Abs(speedDiff) * accelerationRate *
                          Mathf.Sign(speedDiff);
         rb.AddForce(movement * Vector2.right);
@@ -139,13 +139,13 @@ public class Player : Character, IFlashable
     protected override void HandleJumpAnimationEvent()
     {
         // TODO - Player jump on specific keyframe
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 
     /// Strengthens the RigidBody2D fall gravity by a factor of `fallGravityMultiplier`
     private void IncreaseFallGravity()
     {
-        rb.gravityScale = _legacyGravityScale * fallGravityMultiplier;
+        rb.gravityScale = _legacyGravityScale * _fallGravityMultiplier;
     }
 
     /// Resets the RigidBody2D's gravity scale to its original value
@@ -188,16 +188,16 @@ public class Player : Character, IFlashable
 
     public IEnumerator FlashEffect()
     {
-        for (int i = 0; i < flashCount; i++)
+        for (int i = 0; i < _flashCount; i++)
         {
             // Make the sprite red
-            _sr.color = flashColor;
-            yield return new WaitForSeconds(this.flickDuration);
+            _sr.color = _flashColor;
+            yield return new WaitForSeconds(this._flickDuration);
 
             // Make the sprite opaque
             // ReSharper disable once Unity.InefficientPropertyAccess
             _sr.color = new Color(1f, 1f, 1f, 1f);
-            yield return new WaitForSeconds(this.flickDuration);
+            yield return new WaitForSeconds(this._flickDuration);
         }
 
         this._isInvincible = false;
