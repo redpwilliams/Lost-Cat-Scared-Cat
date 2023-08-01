29600f262e73f4f21c4cb3eb7f3ff8cefbbae0ab
Red Williams <redpwilliams@gmail.com>
2023-08-01 01:10:55 -0500

    refactor Parallax scripts

--- Assets/Scripts/Game Managers/BackgroundManager.cs
+++ Assets/Scripts/Game Managers/BackgroundManager.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.Serialization;
 
 public sealed class BackgroundManager : MonoBehaviour
 {
--- Assets/Scripts/UI/Parallax.cs
+++ Assets/Scripts/UI/Parallax.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+public sealed class Parallax : MonoBehaviour
 {
   /// The sprite's length
-  private float spriteLength;
+  private float _spriteLength;
 
   /// How much parallax to add / Parallax coefficient
-  [SerializeField] private float parallaxEffect;
-  [SerializeField] private new Camera camera;
-  
-  private new Transform transform;
+  [SerializeField] private float _parallaxEffect;
+
+  private Camera _cam;
+  private Transform _trans;
   
   /// The number of backgrounds in sequence, left to right
-  private int numBackgrounds;
+  private int _numBackgrounds;
 
   /// If the sprite is visible/off screen
-  private bool isVisible;
+  private bool _isVisible;
 
   /// The calculated background speed.
   /// Product of the BGM scroll velocity and the parallax effect
@@ -23,16 +23,17 @@ public class Parallax : MonoBehaviour
 
   private void Awake()
   {
-    spriteLength = GetComponent<SpriteRenderer>().bounds.size.x;
-    transform = GetComponent<Transform>();
-    isVisible = true;
+    _spriteLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    _trans = transform;
+    _cam = Camera.main;
+    _isVisible = true;
   }
 
   private void Start()
   {
-    numBackgrounds = BackgroundManager.BackgroundCount;
+    _numBackgrounds = BackgroundManager.BackgroundCount;
     ParallaxBackgroundSpeed =
-      BackgroundManager.bgm.ScrollVelocity * this.parallaxEffect;
+      BackgroundManager.bgm.ScrollVelocity * _parallaxEffect;
   }
 
   private void Update()
@@ -41,29 +42,31 @@ public class Parallax : MonoBehaviour
     Vector3 currentPosition = transform.position;
 
     // Set its new position
-    this.transform.position =
+    _trans.position =
       new Vector3(currentPosition.x - ParallaxBackgroundSpeed * Time.deltaTime,
         currentPosition.y, currentPosition.z);
     
     // If object is visible in the camera & not out-of-bounds
-    if (this.isVisible || !(transform.position.x < camera.transform.position.x)) return;
+    if (_isVisible || !(transform.position.x < _cam.transform.position.x))
+     return;
     
     // Move sprite the number of background sets + 1 positions over
     // (to cover for BackgroundManager's possible additional sprites)
-    currentPosition = this.transform.position;
+    currentPosition = _trans.position; 
     
     // And set
-   transform.position = new Vector3(currentPosition.x + (this.numBackgrounds + 1) * this.spriteLength,
-      currentPosition.y, currentPosition.z);
+    _trans.position = new Vector3(
+        currentPosition.x + (_numBackgrounds + 1) * _spriteLength,
+        currentPosition.y, currentPosition.z);
   }
 
   private void OnBecameInvisible()
   {
-    isVisible = false;
+    _isVisible = false;
   }
 
   private void OnBecameVisible()
   {
-    isVisible = true;
+    _isVisible = true;
   }
 }
