df3d610223ef5fadeb8f85a6a2fdf6e6e557ddfb
Red Williams <redpwilliams@gmail.com>
2023-08-01 01:15:18 -0500

    refactor character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,14 +11,14 @@ using UnityEngine.U2D.Animation;
 public abstract class Character : MonoBehaviour
 {
   protected Rigidbody2D rb;
-  private Animator anim;
-  private new Transform transform;
+  private Animator _anim;
+  private Transform _transform;
 
   protected bool IsGrounded { get; private set; }
 
   protected bool IsVisiblyJumping { get; set; }
 
-  private bool isVisiblyRunning;
+  private bool _isVisiblyRunning;
 
   protected bool IsVisiblyRunning { get; set; }
 
@@ -33,21 +33,21 @@ public abstract class Character : MonoBehaviour
   protected virtual void Awake()
   {
     rb = GetComponent<Rigidbody2D>();
-    anim = GetComponent<Animator>();
-    transform = GetComponent<Transform>();
+    _anim = GetComponent<Animator>();
+    _transform = GetComponent<Transform>();
 
-    this.IsGrounded = true;
-    this.HasInputJump = false;
-    this.IsVisiblyJumping = false;
+    IsGrounded = true;
+    HasInputJump = false;
+    IsVisiblyJumping = false;
   }
   /// Returns whether or not the character is running
   protected virtual bool IsRunning()
   {
-    return this.IsGrounded && Mathf.Abs(this.rb.velocity.x) > 0.1f;
+    return IsGrounded && Mathf.Abs(this.rb.velocity.x) > 0.1f;
   }
 
   /// Returns the bool of the Character jumping 
-  /// This is independent from the animation (animation relies on this/math)
+  /// This is independent from the animation (animation relies on math)
   protected virtual bool IsJumping()
   {
     return rb.velocity.y > 0.01f;
@@ -64,8 +64,8 @@ public abstract class Character : MonoBehaviour
   {
     float idleSpeed = BackgroundManager.bgm.ScrollVelocity * 1.1f;
 
-    Vector3 currentPosition = transform.position;
-    this.transform.position = new Vector3(
+    Vector3 currentPosition = _transform.position;
+    _transform.position = new Vector3(
       currentPosition.x - idleSpeed * Time.deltaTime, currentPosition.y,
       currentPosition.z);
   }
@@ -76,42 +76,42 @@ public abstract class Character : MonoBehaviour
   /// Sets the passed `IsRunning` parameter in the Animator tab
   protected void SetRunAnimationParam(bool value)
   {
-    this.anim.SetBool(Running, value);
+    _anim.SetBool(Running, value);
   }
 
   /// Sets the passed `IsJumping` parameter in the Animator tab
   protected void SetJumpAnimationParam(bool value)
   {
-    this.anim.SetBool(Jumping, value);
+    _anim.SetBool(Jumping, value);
   }
 
   /// Sets the passed `IsFalling` parameter in the Animator tab
   protected void SetFallAnimationParam(bool value)
   {
-    this.anim.SetBool(Falling, value);
+    _anim.SetBool(Falling, value);
   }
   
   /// Sets the passed `IsCrouching` parameter in the Animator tab
   protected void SetCrouchingAnimationParam(bool value)
   {
-    this.anim.SetBool(Crouching, value);
+    _anim.SetBool(Crouching, value);
   }
 
   /// Sets the passed `IsDashing` parameter in the Animator tab
   protected void SetDashingAnimationParam(bool value)
   {
-    this.anim.SetBool(Dashing, value);
+    _anim.SetBool(Dashing, value);
   }
   
-  /// Called when this object collides with something
+  /// Called when object collides with something
   private void OnCollisionEnter2D()
   {
-    this.IsGrounded = true;
+    IsGrounded = true;
   }
 
-  /// Called when this object leaves a collider
+  /// Called when object leaves a collider
   private void OnCollisionExit2D()
   {
-    this.IsGrounded = false;
+    IsGrounded = false;
   }
 }
